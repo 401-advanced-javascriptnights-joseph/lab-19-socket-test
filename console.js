f850e5854eeaffdab4f94c0e5ba19a5a6adf1cd5
JCode1986 <joseph.hangarter@yahoo.com>
2019-08-17 21:18:14 -0700

    working

--- console.js
+++ console.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const theword = require('theword.js');
+const theWord = require('./theword');
 
 const io = require('socket.io-client');
 
 const socket = io.connect('http://localhost:3000');
 
-socket.on('the-bird', theword);
+socket.on('the-bird', theWord);
 
